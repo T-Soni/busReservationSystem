1900363a01d88d1ade66bd10950be1f029a79e48
Tanushree <tssoni1206@gmail.com>
2025-04-16 00:21:20 +0530

    updated seatBooking logic

--- server/server_func_implementation.cpp
+++ server/server_func_implementation.cpp
@@ -6,25 +6,37 @@ using namespace std;
 map<string, User> users;
 map<string, Bus> buses = {
     {"Volvo", Bus("Volvo", 20, 500)},
-    {"Express", Bus("Express", 30, 300)}
-};
+    {"Express", Bus("Express", 30, 300)}};
 mutex userMutex, busMutex;
 
-void handleClient(int clientSocket) {
+void handleClient(int clientSocket)
+{
     int code;
-    while (read(clientSocket, &code, sizeof(code)) > 0) {
-        switch (code) {
-            case REGISTRATION_CODE: registerUser(clientSocket); break;
-            case LOGIN_CODE: loginUser(clientSocket); break;
-            case VIEW_BUSES_CODE: viewBuses(clientSocket); break;
-            case BOOK_SEAT_CODE: bookSeat(clientSocket); break;
-            case SHOW_TICKETS_CODE: {
-                char name[100];
-                read(clientSocket, name, sizeof(name));
-                sendTickets(clientSocket, name);
-                break;
-            }
-            default: break;
+    while (read(clientSocket, &code, sizeof(code)) > 0)
+    {
+        switch (code)
+        {
+        case REGISTRATION_CODE:
+            registerUser(clientSocket);
+            break;
+        case LOGIN_CODE:
+            loginUser(clientSocket);
+            break;
+        case VIEW_BUSES_CODE:
+            viewBuses(clientSocket);
+            break;
+        case BOOK_SEAT_CODE:
+            bookSeat(clientSocket);
+            break;
+        case SHOW_TICKETS_CODE:
+        {
+            char name[100];
+            read(clientSocket, name, sizeof(name));
+            sendTickets(clientSocket, name);
+            break;
+        }
+        default:
+            break;
         }
     }
     close(clientSocket);
@@ -39,26 +51,30 @@ void handleClient(int clientSocket) {
 //     int success = 1;
 //     write(clientSocket, &success, sizeof(success));
 // }
-string readString(int sock) {
+string readString(int sock)
+{
     string result;
     char ch;
-    while (read(sock, &ch, 1) > 0 && ch != '\0') {
+    while (read(sock, &ch, 1) > 0 && ch != '\0')
+    {
         result += ch;
     }
     return result;
 }
 
-void registerUser(int clientSocket) {
+void registerUser(int clientSocket)
+{
     string name = readString(clientSocket);
     string pass = readString(clientSocket);
     lock_guard<mutex> lock(userMutex);
     users[name] = {name, pass};
+    saveUsersToFile();
     int success = 1;
     write(clientSocket, &success, sizeof(success));
 }
 
-
-void loginUser(int clientSocket) {
+void loginUser(int clientSocket)
+{
     // char name[100], pass[100];
     // read(clientSocket, name, sizeof(name));
     // read(clientSocket, pass, sizeof(pass));
@@ -69,24 +85,28 @@ void loginUser(int clientSocket) {
     write(clientSocket, &success, sizeof(success));
 }
 
-void viewBuses(int clientSocket) {
+void viewBuses(int clientSocket)
+{
     string result = "Available Buses:\n";
-    for (auto& [name, bus] : buses) {
+    for (auto &[name, bus] : buses)
+    {
         result += "-----------------------------\n";
         result += "Bus: " + name + " | Price: Rs." + to_string(bus.price) + "\n";
         result += "Seat Matrix (X = Booked, O = Free):\n";
         int seatCount = bus.seats.size();
-        for (int i = 0; i < seatCount; ++i) {
+        for (int i = 0; i < seatCount; ++i)
+        {
             result += (bus.seats[i] ? "X " : "O ");
-            if ((i + 1) % 5 == 0) result += "\n"; // 5 seats per row
+            if ((i + 1) % 5 == 0)
+                result += "\n"; // 5 seats per row
         }
         result += "\n-----------------------------\n";
     }
     send(clientSocket, result.c_str(), result.size(), 0);
 }
 
-
-void bookSeat(int clientSocket) {
+void bookSeat(int clientSocket)
+{
     char busname[100], username[100];
     int seat;
     read(clientSocket, busname, sizeof(busname));
@@ -94,33 +114,79 @@ void bookSeat(int clientSocket) {
     read(clientSocket, &seat, sizeof(seat));
     lock_guard<mutex> lock(busMutex);
 
-    if (buses[busname].seats[seat] == false) {
+    // if (buses[busname].seats[seat] == false) {
+    //     buses[busname].seats[seat] = true;
+    //     users[username].bookedSeats.push_back(seat);
+    //     int success = BOOKING_SUCCESS_CODE;
+    //     write(clientSocket, &success, sizeof(success));
+    // } else {
+    //     int fail = SERVER_ERROR_CODE;
+    //     write(clientSocket, &fail, sizeof(fail));
+    // }
+    // Check if the bus exists
+    if (buses.find(busname) == buses.end())
+    {
+        int fail = SERVER_ERROR_CODE;
+        write(clientSocket, &fail, sizeof(fail));
+        return;
+    }
+
+    // Check if seat number is valid
+    // if (seat < 0 || seat >= buses[busname].seats.size())
+    if (seat < 0 || seat >= static_cast<int>(buses[busname].seats.size()))
+
+    {
+        int fail = SERVER_ERROR_CODE;
+        write(clientSocket, &fail, sizeof(fail));
+        return;
+    }
+
+    if (!buses[busname].seats[seat])
+    {
         buses[busname].seats[seat] = true;
+
+        // In case the user doesn’t exist, add them
+        if (users.find(username) == users.end())
+        {
+            users[username] = User{username, {}, {}};
+        }
+
         users[username].bookedSeats.push_back(seat);
+        saveUsersToFile();
+        saveBusesToFile();
+
         int success = BOOKING_SUCCESS_CODE;
         write(clientSocket, &success, sizeof(success));
-    } else {
+    }
+    else
+    {
         int fail = SERVER_ERROR_CODE;
         write(clientSocket, &fail, sizeof(fail));
     }
+    cout << "[BOOKING] User: " << username << " | Bus: " << busname << " | Seat: " << seat << endl;
 }
 
-void sendTickets(int clientSocket, const string& username) {
+void sendTickets(int clientSocket, const string &username)
+{
     string msg = "Your Booked Seats:\n";
-    for (int seat : users[username].bookedSeats) {
+    for (int seat : users[username].bookedSeats)
+    {
         msg += "Seat " + to_string(seat) + "\n";
     }
     send(clientSocket, msg.c_str(), msg.size(), 0);
 }
-void loadUsersFromFile() {
+void loadUsersFromFile()
+{
     ifstream fin("users.txt");
     string uname, pass;
     int n;
-    while (fin >> uname >> pass >> n) {
+    while (fin >> uname >> pass >> n)
+    {
         User u;
         u.username = uname;
         u.password = pass;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             int seat;
             fin >> seat;
             u.bookedSeats.push_back(seat);
@@ -130,23 +196,29 @@ void loadUsersFromFile() {
     fin.close();
 }
 
-void saveUsersToFile() {
+void saveUsersToFile()
+{
     ofstream fout("users.txt");
-    for (auto& [uname, user] : users) {
+    for (auto &[uname, user] : users)
+    {
         fout << uname << " " << user.password << " " << user.bookedSeats.size();
-        for (int seat : user.bookedSeats) fout << " " << seat;
+        for (int seat : user.bookedSeats)
+            fout << " " << seat;
         fout << "\n";
     }
     fout.close();
 }
 
-void loadBusesFromFile() {
+void loadBusesFromFile()
+{
     ifstream fin("buses.txt");
     string name;
     int totalSeats, price;
-    while (fin >> name >> totalSeats >> price) {
+    while (fin >> name >> totalSeats >> price)
+    {
         Bus b(name, totalSeats, price);
-        for (int i = 0; i < totalSeats; ++i) {
+        for (int i = 0; i < totalSeats; ++i)
+        {
             int val;
             fin >> val;
             b.seats[i] = (val == 1);
@@ -156,14 +228,15 @@ void loadBusesFromFile() {
     fin.close();
 }
 
-void saveBusesToFile() {
+void saveBusesToFile()
+{
     ofstream fout("buses.txt");
-    for (auto& [name, bus] : buses) {
+    for (auto &[name, bus] : buses)
+    {
         fout << name << " " << bus.seats.size() << " " << bus.price;
-        for (bool s : bus.seats) fout << " " << s;
+        for (bool s : bus.seats)
+            fout << " " << s;
         fout << "\n";
     }
     fout.close();
 }
-
-
